7eef80bb56661565236555e6e14d50f8b56c2031
MD2020m <dmarshall2019@gmail.com>
2025-11-02 21:23:28 -0600

    Write functions in healthReader.js and workoutReader.js

---
+++ healthReader.js
@@ -0,0 +1,23 @@
+const fs = require('fs/promises');
+
+// Reads health-metrics.json
+async function ReadHealthMetrics(filepath) {
+    try {
+        const data = await fs.readFile(filepath, 'utf8');
+        const healthData = JSON.parse(data);
+        console.log(`Total health entries: ${healthData.metrics.length}`);
+        return healthData;
+    } catch(error) {
+        if (error.code === 'ENOENT') {
+            console.log('File not found. Check file path');
+        } else if (error.name === 'SyntaxError') {
+            console.log('Invlaid JSON. Check file format');
+        } else {
+            console.log('Unknown error:', error.message);
+        }
+        return null;
+    }
+}
+
+let filepath = './data/health-metrics.json';
+ReadHealthMetrics();
---
+++ workoutReader.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+
+async function ReadWorkoutData(filepath) {
+    return new Promise((resolve, reject) => {
+        const results = [];
+
+        fs.createReadStream(filepath)
+            .pipe(csv())
+            .on('data', (row) => {
+                results.push(row);
+            })
+            .on('end', () => {
+                resolve(results);
+            })
+            .on('error', (error) => {
+                reject(error);
+            });
+    });
+}
+
+async function ProcessWorkoutFile(filepath) {
+    try {
+        const data = await ReadWorkoutData(filepath);
+        let numWorkouts = data.length;
+        let totalMinutes = 0;
+        for (let i = 0; i < data.length; i++) {
+            const workout = data[i];
+            totalMinutes += parseFloat(workout.duration);
+        }
+        return { numWorkouts, totalMinutes };
+    } catch(error) {
+        if (error.code == 'ENOENT') {
+            console.log('CSV file not found. Check file path');
+        } else {
+            console.log('Error processing CSV file:', error.message);
+        }
+        return null;
+    }
+}
+
+let filename = './data/workouts.csv'
+console.log(ProcessWorkoutFile(filename));
